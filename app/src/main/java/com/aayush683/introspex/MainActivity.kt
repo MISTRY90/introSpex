e77f0835905a1781cb74acc8b74159d785e262dd
Aayush Goel <75422863+Aayush-683@users.noreply.github.com>
2024-09-23 12:20:22 +0530

    Added Temperature Chart and Memory Pie Chart

--- app/src/main/java/com/aayush683/introspex/MainActivity.kt
+++ app/src/main/java/com/aayush683/introspex/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.aayush683.introspex
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.Debug.MemoryInfo
 import android.os.Handler
 import android.os.Looper
 import android.widget.TextView
@@ -15,10 +17,16 @@ import androidx.core.view.WindowInsetsCompat
 import com.an.deviceinfo.device.model.Battery
 import com.an.deviceinfo.location.LocationInfo
 import com.an.deviceinfo.device.model.Device
+import com.an.deviceinfo.device.model.Memory
 import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.charts.PieChart
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.oseamiya.deviceinformation.MemoryInformation
 
 
 class MainActivity : AppCompatActivity() {
@@ -27,86 +35,74 @@ class MainActivity : AppCompatActivity() {
         private const val LOCATION_PERMISSION_REQUEST_CODE = 1
     }
 
+    private lateinit var phoneTempChart: LineChart
+    private lateinit var usedMemChart: PieChart
+    private val tempArr = ArrayList<Entry>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(R.layout.home)
 
-        // Check for location permission
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-            != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                LOCATION_PERMISSION_REQUEST_CODE)
-        } else {
-            ShowData()
-        }
+        // Initialize UI components here
+        phoneTempChart = findViewById(R.id.phoneTempChart)
+        usedMemChart = findViewById(R.id.usedMemChart)
+
+        ShowData()
     }
-
-    private val tempArr = ArrayList<Entry>();
 
     private fun ShowData() {
-        val locationInfo = LocationInfo(this)
+        setContentView(R.layout.home)
+
+        // Find the views after setting the correct layout
+        phoneTempChart = findViewById(R.id.phoneTempChart)
+        usedMemChart = findViewById(R.id.usedMemChart)
+
+        // Setup Charts
+        phoneTempChart.setTouchEnabled(false)
+        phoneTempChart.setPinchZoom(false)
+        phoneTempChart.setBackgroundResource(R.drawable.vector)
+
+        usedMemChart.setTouchEnabled(false)
+        usedMemChart.setBackgroundResource(R.drawable.vector)
+
+        // Update charts with data
         val battery = Battery(this)
-        val device=Device(this)
-        val location = locationInfo.location
-        val addr = location.addressLine1
-        val city = location.city
-        val state = location.state
-        val country = location.countryCode
+        val temp = battery.batteryTemperature
+        updateTempChart(temp, phoneTempChart)
 
-        val model= device.model
-
-        val textBox1 = findViewById<TextView>(R.id.textBox1)
-        val textBox2=findViewById<TextView>(R.id.textBox2)
-        val batteryTempChart = findViewById<LineChart>(R.id.batteryTempChart)
-
-        val text = "Your Location: ${addr}, ${city}, ${state} - ${country}"
-        textBox1.text = text
-
-        val text1="Device Model: ${model}"
-        textBox2.text=text1
-
-
-
-        // Add a 2 second interval
-        val handler = Handler(Looper.getMainLooper())
-        val runnable = object : Runnable {
-            override fun run() {
-                val t = battery.batteryTemperature
-                updateBatteryTempChart(t, batteryTempChart)
-                handler.postDelayed(this, 2000)
-            }
-        }
-        handler.post(runnable)
+        val memory = MemoryInformation(this)
+        updateMemChart(memory, usedMemChart)
     }
 
-    private fun updateBatteryTempChart(temperature: Float, batteryTempChart: LineChart) {
+    private fun updateTempChart(temperature: Float, phoneTempChart: LineChart) {
         tempArr.add(Entry(tempArr.size.toFloat(), temperature))
         if (tempArr.size > 10) tempArr.removeAt(0)
-        val lineDataSet = LineDataSet(tempArr, "Battery Temperature")
-        lineDataSet.color = ContextCompat.getColor(this@MainActivity, R.color.cyan)
-        lineDataSet.valueTextColor = ContextCompat.getColor(this@MainActivity, R.color.white)
-        batteryTempChart.data = LineData(lineDataSet)
-        batteryTempChart.invalidate()
+        val lineDataSet = LineDataSet(tempArr, "Device Temperature")
+        lineDataSet.color = ContextCompat.getColor(this, R.color.cyan)
+        lineDataSet.valueTextColor = ContextCompat.getColor(this, R.color.white)
+        phoneTempChart.data = LineData(lineDataSet)
+        phoneTempChart.description.text = "Device Temperature"
+        phoneTempChart.description.textColor = ContextCompat.getColor(this, R.color.white)
+        phoneTempChart.invalidate()
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int, permissions: Array<out String>, grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
-            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                ShowData()
-            } else {
-                val textBox1 = findViewById<TextView>(R.id.textBox1)
-                textBox1.text = "Location permission denied."
-            }
-        }
+    private fun updateMemChart(memory: MemoryInformation, usedMemChart: PieChart) {
+        val totalMem = memory.totalRam
+        val usedMem = memory.usedRam
+        val usedMemPercentage = (usedMem.toFloat() / totalMem.toFloat()) * 100
+        val freeMemPercentage = 100 - usedMemPercentage
+
+        // Plot data in pie chart
+        val pieEntries = ArrayList<PieEntry>()
+        pieEntries.add(PieEntry(usedMemPercentage, "Used"))
+        pieEntries.add(PieEntry(freeMemPercentage, "Free"))
+        val pieDataSet = PieDataSet(pieEntries, "")
+        pieDataSet.colors = listOf(ContextCompat.getColor(this, R.color.cyan), ContextCompat.getColor(this, R.color.white))
+        usedMemChart.data = PieData(pieDataSet)
+        usedMemChart.description.text = "Memory Usage"
+        usedMemChart.description.textColor = ContextCompat.getColor(this, R.color.white)
+        usedMemChart.invalidate()
     }
 }
 
